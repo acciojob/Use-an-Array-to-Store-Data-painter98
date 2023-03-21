06bc48e76ba8ed67dea6fdf39f520e0d4d204be9
painter98 <120320713+painter98@users.noreply.github.com>
2023-03-21 10:27:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,35 @@
-let yourArray[ "hello","world",3,4.5,true];
-console.log(yourArray.length);
+let yourArray = [
+[
+	{
+		one: "hii",
+		two: 90,
+		three: true
+	}
+],
+[
+		four: "hello",
+		five: 10,
+		six: false
+],
+[
+	{
+		a: "welcome",
+		b: 80,
+		c: true
+	}
+],
+[
+	{
+		d: "here",
+		e: 40,
+		f: false
+	}
+]
+[
+	{
+		g: "again",
+		h: 56,
+		i: true
+	}
+]
+];
